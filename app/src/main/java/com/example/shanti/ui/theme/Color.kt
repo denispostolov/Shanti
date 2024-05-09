e60d63259bf1f6a71b1af13d607227485be02821
alessandrosodu <blackalex983@gmail.com>
2024-05-09 11:39:39 +0200

    add app social color

--- app/src/main/java/com/example/shanti/ui/theme/Color.kt
+++ app/src/main/java/com/example/shanti/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xB177D2)
+val BackgroundColor = Color(0xF7F5F9)
+val AccentColor = Color(0x4a196d)
