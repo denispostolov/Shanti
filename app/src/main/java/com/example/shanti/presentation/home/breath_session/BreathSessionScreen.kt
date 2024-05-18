4a4a5c6ea65e1a29dc30e72b9d1e6334228f50fb
alessandrosoddu <alessandro.soddu@edu.unito.it>
2024-05-18 22:38:28 +0200

    initial setting for Breath Session

--- app/src/main/java/com/example/shanti/presentation/home/breath_session/BreathSessionScreen.kt
+++ app/src/main/java/com/example/shanti/presentation/home/breath_session/BreathSessionScreen.kt
@@ -1,9 +1,59 @@
 package com.example.shanti.presentation.home.breath_session
 
-import androidx.compose.material3.Text
+import android.os.VibrationEffect
+import android.os.Vibrator
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BreathSessionScreen() {
-    Text("Breath Session")
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.5f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ), label = ""
+    )
+
+    val context = LocalContext.current
+    val vibrator = context.getSystemService(Vibrator::class.java)
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+            kotlinx.coroutines.delay(4000) // Wait for one full breath cycle (2 seconds in, 2 seconds out)
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .scale(scale)
+                .background(Color.Cyan, shape = CircleShape)
+        )
+    }
 }
