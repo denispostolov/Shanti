06454e92ae1cedc33b4f00558fc36e6a7ba433e4
alessandrosoddu <alessandro.soddu@edu.unito.it>
2024-05-29 22:04:46 +0200

    jUnit testing for defineStatus

--- app/src/main/java/com/example/shanti/test/UtilsTest.kt
+++ app/src/main/java/com/example/shanti/test/UtilsTest.kt
@@ -1,11 +1,14 @@
 package com.example.shanti.test
 
+import com.example.shanti.common.defineStatus
 import com.example.shanti.common.getTrainerNameAndSurnameFromFullName
+import com.example.shanti.domain.model.Status
 import junit.framework.Assert.assertEquals
 import org.junit.Test
+import java.time.LocalTime
+import java.util.Date
 
-class MathUtilsTest {
-
+class UtilsTest {
     @Test
     fun testGetTrainerNameAndSurnameFromFullNameWithFullName() {
         val fullName = "John Doe"
@@ -41,3 +44,46 @@ class MathUtilsTest {
         assertEquals(Pair("John", "Doe"), result)
     }
 }
+
+class DefineStatusTest {
+    @Test
+    fun testDefineStatusWithFutureDate() {
+        val futureDate = Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000) // Tomorrow
+        val time = LocalTime.now()
+        val result = defineStatus(futureDate, time)
+        assertEquals(Status.FUTURE, result)
+    }
+
+    @Test
+    fun testDefineStatusWithPastDate() {
+        val pastDate = Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000) // Yesterday
+        val time = LocalTime.now()
+        val result = defineStatus(pastDate, time)
+        assertEquals(Status.PASSED, result)
+    }
+
+    @Test
+    fun testDefineStatusWithTodayDateAndFutureTime() {
+        val todayDate = Date()
+        val futureTime = LocalTime.now().plusHours(1)
+        val result = defineStatus(todayDate, futureTime)
+        assertEquals(Status.FUTURE, result)
+    }
+
+    @Test
+    fun testDefineStatusWithTodayDateAndPastTime() {
+        val todayDate = Date()
+        val pastTime = LocalTime.now().minusHours(1)
+        val result = defineStatus(todayDate, pastTime)
+        assertEquals(Status.PASSED, result)
+    }
+
+    @Test
+    fun testDefineStatusWithTodayDateAndCurrentTime() {
+        val todayDate = Date()
+        val currentTime = LocalTime.now()
+        val result = defineStatus(todayDate, currentTime)
+        assertEquals(Status.FUTURE, result)
+    }
+}
+
